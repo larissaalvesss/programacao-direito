5bed16b938234ecb3310005d37f314c144f0cf58
Larissa Alves <141174221+larissaalvesss@users.noreply.github.com>
2023-10-24 14:49:07 -0300

    Add files via upload

---
+++ trabalha phytonn.py
@@ -0,0 +1,26 @@
+def soma (x, y):
+    return x + y 
+
+print(soma(3, 100))
+print(soma(3, 7343284328748234))
+
+
+def simples (cor):
+    if cor == 'rosa':
+        return 'escolheu certo'
+    
+def completo (cor):
+    if cor == 'rosa':
+        return 'vc e linda'
+    elif cor == 'roxo':
+        return 'vc e top'
+    else:
+        return 'vsf ein'
+    
+def medio (cor):
+    if cor == 'azul':
+      return 'escolheu certo'
+    else: 
+      return 'tente dnv'
+    
+        
